eda64c29c2c24b33995ac84a256883c4cfca9aee
boudi425 <bodybrkt@gmail.com>
2025-12-18 12:30:20 +0300

    added new dark mode button, fixed some smaller screens issues

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
 let Body = document.querySelector("body");
-let changeMode = document.querySelector(".mode");
+const changeMode = document.querySelector("#themeToggle");
 let heroTXT = document.querySelector(".heroGreetings");
 const Form = document.querySelector(".Contact form");
 const sections = document.querySelectorAll(".observe");
-Body.dataset.theme = localStorage.getItem("Mode");
+Body.dataset.theme = localStorage.getItem("theme");
 Form.addEventListener("submit", (e) => {
   Form.reset();
 });
 changeMode.addEventListener("click", () => {
-  Body.dataset.theme = Body.dataset.theme === "light" ? "dark" : "light";
-  if (changeMode.classList.contains("fa-moon")) {
-    changeMode.classList.remove("fa-moon");
-    localStorage.setItem("Mode", "dark");
-    changeMode.classList.add("fa-sun");
-  } else {
-    changeMode.classList.remove("fa-sun");
-    localStorage.setItem("Mode", "light");
-    changeMode.classList.add("fa-moon");
-  }
-});
+  const nextTheme = Body.dataset.theme === "dark" ? "light" : "dark";
 
+  Body.dataset.theme = nextTheme;
+  localStorage.setItem("theme", nextTheme);
+});
 const mainTXT = "Hi, I am Abdelrahaman";
 setTimeout(() => {
   for (let i = 0; i < mainTXT.length; i++) {
